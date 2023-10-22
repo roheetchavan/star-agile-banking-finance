a32257a4ca50042ece7272db5847a1c4c944f35d
Rohit Chavan <roheetchavan@gmail.com>
2023-10-22 13:02:06 +0530

    Corrected terraform file
    Signed-off-by: Rohit Chavan <roheetchavan@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,94 +1,100 @@
-# Define the provider (AWS)
 provider "aws" {
-  region = "us-east-2" # Change this to your desired AWS region
-  access_key = "AKIASA625THS65UB6YFT"
-  secret_key = "Klx03fVC+4PAWeVYOa5DbwOxfKE8hExDjnXAQfbG"
+  region = "us-east-2"
+  shared_credentials_files = ["~/.aws/credentials"]
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.22.0"
+    }
+  }
 }
 
-# Create an internet gateway
-resource "aws_internet_gateway" "example" {
-  vpc_id = aws_vpc.example.id
+
+#resource "aws_key_pair" "key" {
+#  key_name   = "id_rsa"
+#  public_key = file("~/.ssh/id_rsa.pub")
+#}
+
+resource "aws_default_vpc" "default_vpc" {
+
 }
 
-# Create a custom route table
-resource "aws_route_table" "example" {
-  vpc_id = aws_vpc.example.id
-}
+resource "aws_security_group" "allow_traffic" {
+  name        = "allow_traffic"
+  description = "Allow inbound traffic"
 
-# Create a subnet
-resource "aws_subnet" "example" {
-  vpc_id                  = aws_vpc.example.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-2a" # Change this to your desired availability zone
-  map_public_ip_on_launch = true
-}
-
-# Associate the subnet with the custom route table
-resource "aws_route_table_association" "example" {
-  subnet_id      = aws_subnet.example.id
-  route_table_id = aws_route_table.example.id
-}
-
-# Create a security group
-resource "aws_security_group" "example" {
-  name_prefix = "example-"
-  description = "Example security group"
-
-  # Allow SSH (Port 22), HTTP (Port 80), and HTTPS (Port 443) traffic
+  # using default VPC
+  vpc_id      = aws_default_vpc.default_vpc.id
   ingress {
+    description = "TLS from VPC"
+
+    # we should allow incoming and outoging
+    # TCP packets
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
+
+    # allow all traffic
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    description = "Allow 8081"
+
+    # we should allow incoming and outoging
+    # TCP packets
+    from_port   = 8081
+    to_port     = 8081
     protocol    = "tcp"
+
+    # allow all traffic
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  vpc_id = aws_vpc.example.id
-}
-
-# Create a network interface
-resource "aws_network_interface" "example" {
-  subnet_id   = aws_subnet.example.id
-}
-
-# Create an EC2 instance
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Ubuntu 20.04 LTS AMI ID, replace with your desired AMI
-  instance_type = "t2.micro"             # Change the instance type if needed
-  key_name      = "TestAWS"              # Replace with your key pair name
-
-  network_interface {
-    network_interface_id = aws_network_interface.example.id
-    device_index = 0
-  }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo apt-get update -y
-              sudo apt-get install apache2 -y
-              sudo systemctl start apache2
-              sudo systemctl enable apache2
-              EOF
 
   tags = {
-    Name = "ExampleEC2Instance2"
+    Name = "allow_traffic"
   }
+}
+
+variable "ami_id" {
+  description = "this is ubuntu ami id"
+  default     = "ami-0c55b159cbfafe1f0"
+}
+
+resource "aws_instance" "my_ec2" {
+  ami             = var.ami_id
+  instance_type   = "t2.micro"
+
+  # refering key which we created earlier
+  key_name        = "TestAWS"
+
+  # refering security group created earlier
+  security_groups = [aws_security_group.allow_traffic.name]
+
+  tags = {
+    Name = "my-test-ec2"
+  }
+}
+
+resource "null_resource" "write_hosts_file" {
+  triggers = {
+    public_ip = aws_instance.my_ec2.public_ip
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+rm -f hosts
+echo "[servers]" > hosts
+echo "my-test-ec2 ansible_ssh_host=${aws_instance.my_ec2.public_ip} ansible_ssh_user=ubuntu" >> hosts
+cp hosts ../ansible
+cat ../ansible/hosts
+EOF
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.my_ec2.public_ip
 }
